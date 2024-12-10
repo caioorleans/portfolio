e46466ed0e657de52c0d53810c2e5010717d9600
Caio Orleans <caioorleansdm@gmail.com>
2024-12-10 11:31:16 -0300

    Added responsivity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { ContentComponent } from './content/content.component';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, ContentComponent],
+  imports: [MenuComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PhotoAndDescriptionComponent } from "../photo-and-description/photo-and-description.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [PhotoAndDescriptionComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
