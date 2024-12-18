9882d5e9fa174c0d5eac77ad266272209486a9c2
Caio Orleans <caioorleansdm@gmail.com>
2024-12-18 16:54:33 -0300

    menu closing when selecting an item

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { PhotoAndDescriptionComponent } from '../photo-and-description/photo-and-description.component';
 
 @Component({
@@ -9,5 +9,9 @@ import { PhotoAndDescriptionComponent } from '../photo-and-description/photo-and
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  @Output() emitterCloseMenu = new EventEmitter<void>();
 
+  notifyCloseMenu(){
+    this.emitterCloseMenu.emit();
+  }
 }
