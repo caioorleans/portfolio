921ebb918fee25ee6e2095a0a291c8cc8a9b394a
Caio Orleans <caioorleansdm@gmail.com>
2024-12-18 16:31:46 -0300

    mobile menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContentComponent } from './components/content/content.component';
-import { isPlatformBrowser } from '@angular/common';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MenuComponent, ContentComponent],
+  imports: [CommonModule,MenuComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
+
+  showMenu = false;
+
+  changeMenuState(){
+    this.showMenu = !this.showMenu
+  }
 }
