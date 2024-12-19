6c7f3a9ba681104555eae6be1f68d9eaf99933ff
Caio Orleans <caioorleansdm@gmail.com>
2024-12-18 22:23:19 -0300

    Text decoration in about me

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { PhotoAndDescriptionComponent } from '../photo-and-description/photo-and-description.component';
 import { SectionComponent } from '../section/section.component';
 import { QualificationItemComponent } from '../qualification-item/qualification-item.component';
 import { CommonModule } from '@angular/common';
 import { ExperienceItemComponent } from "../experience-item/experience-item.component";
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-content',
@@ -19,13 +20,20 @@ import { ExperienceItemComponent } from "../experience-item/experience-item.comp
   styleUrl: './content.component.css'
 })
 export class ContentComponent {
+  htmlContent: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {
+    const rawHtml = '';
+    this.htmlContent = this.sanitizer.bypassSecurityTrustHtml(rawHtml);
+  }
+
   info = {
     biografy : {
       title: "Biografia",
       content: [
         "Me chamo Caio Orleans, tenho 28 anos e sou de Fortaleza/CE. Meu primeiro contato com a programação foi em 2015, quando ingressei no curso de Matemática Industrial, pela Universidade Federal do Ceará (UFC).",
         "Três anos depois, iniciei o curso de Sistemas e Mídias Digitais, também da UFC, no qual tive maior contato com a programação em forma midiática, seja através da criação de sites, jogos, aplicativos e sistemas digitais.",
-        "Nesses quase 10 anos, tive a oportunidade de trabalhar como desenvolvedor Front-End em dois projetos da UFC, como desenvolvedor Back-End em um projeto em parceria com a Prefeitura de Fortaleza e como desenvolvedor Mobile no Instituto Irede."
+        "Nesses quase 10 anos, tive contato com diversas linguagens e tecnologias, entretando as que mantive maior foco foram <span>Spring Boot</span> com Java e <span>NestJs</span> com Typescript para backend, <span>Angular</span> para front-end web e <span>Jetpack Compose</span> com Kotlin para dispositivos Android. Ainda nesse período, tive a oportunidade de trabalhar como desenvolvedor Front-End em dois projetos da UFC, como desenvolvedor Back-End em um projeto em parceria com a Prefeitura de Fortaleza e como desenvolvedor Mobile no Instituto Irede."
       ]
     },
     qualifications: {
@@ -60,22 +68,22 @@ export class ContentComponent {
       title: "Experiências Profissionais",
       content: [
         {
-          position: "Bolsista desenvolvedor front-end no projeto Athena",
+          position: "Bolsista desenvolvedor front-end nos projetos Athena e Aprendizagem Móvel e Colaborativa no Ensino Fundamental",
           company: "Universidade Federal do Ceará",
           description: "Blb bla bla bla bla",
           period: "2020 - 2023"
         },
         {
-          position: "Bolsista desenvolvedor front-end no projeto Athena",
-          company: "Universidade Federal do Ceará",
+          position: "Bolsista desenvolvedor back-end no projeto Juventude Digital na Prática",
+          company: "Casa da Cultura Digital",
           description: "Blb bla bla bla bla",
-          period: "2020 - 2023"
+          period: "2023"
         },
         {
-          position: "Bolsista desenvolvedor front-end no projeto Athena",
-          company: "Universidade Federal do Ceará",
+          position: "Residente em desenvolvimento Android",
+          company: "Instituto Irede",
           description: "Blb bla bla bla bla",
-          period: "2020 - 2023"
+          period: "2024"
         }
       ]
     }
