9558c3fb7e2432daf8c29bc076f4ef67dc70c0ff
Caio Orleans <caioorleansdm@gmail.com>
2024-12-18 21:52:23 -0300

    mobile menu animation

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,15 +1,17 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PhotoAndDescriptionComponent } from '../photo-and-description/photo-and-description.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [PhotoAndDescriptionComponent],
+  imports: [CommonModule,PhotoAndDescriptionComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
   @Output() emitterCloseMenu = new EventEmitter<void>();
+  @Input() isMenuActive?:Boolean
 
   notifyCloseMenu(){
     this.emitterCloseMenu.emit();
