3d4677fe5a8e11b545cf8d415a4b84f0b5df1287
Caio Orleans <caioorleansdm@gmail.com>
2024-12-22 21:50:40 -0300

    animations with gsap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, HostListener } from '@angular/core';
+import { AfterViewInit, Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
 import { ContentComponent } from './components/content/content.component';
-import { CommonModule} from '@angular/common';
+import { CommonModule, isPlatformBrowser} from '@angular/common';
 import { MenuComponent } from './components/menu/menu.component';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,37 @@ import { MenuComponent } from './components/menu/menu.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  
   title = 'portfolio';
 
   showMenu = false;
 
   currentSection?: string = 'about-me';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object){}
+
+  ngAfterViewInit(): void {
+    gsap.registerPlugin(ScrollTrigger)
+    if(isPlatformBrowser(this.platformId)){
+      document.querySelectorAll('.animatable').forEach((item)=>{    
+        ScrollTrigger.create({
+          trigger: item,
+          start: 'bottom 60px', // Quando o item está perto da saída da tela
+          end: 'bottom 40px', // Quando já saiu completamente
+          scrub: true, // Faz a animação ser sincronizada com o scroll
+          onUpdate: (self) => {
+            const progress = self.progress; // Progresso da saída
+            gsap.to(item, {
+              alpha: 1 - progress, // Reduz a opacidade gradualmente
+              y: -25 * progress, // Move o item para cima proporcionalmente
+              overwrite: 'auto', // Evita conflitos de animação
+            });
+          },
+        });
+      })
+    }
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll(): void {
